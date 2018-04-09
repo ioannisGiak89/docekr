046b7535015e7ab651c3103c772324cd5243e565
Ioannis Giakoumidis <ioannis.giakoumidis@york.ac.uk>
2018-04-09 10:31:50 +0100

    Remove comment

--- grails/provisioning/docker-entrypoint.sh
+++ grails/provisioning/docker-entrypoint.sh
@@ -9,7 +9,7 @@ if [ ${#USER} == 0 ] && [ ${#GROUP} == 0 ]; then
     # Create the user with the given home directory.
     echo "    Creating user with UID:GID of ${GRAILS_UID}:${GRAILS_GID}"
     adduser -D -s /bin/bash -u ${GRAILS_UID} -g ${GRAILS_GID} -h ${GRAILS_HOME} grails
-    # addgroup -g ${GRAILS_GID} grails
+
 else
     echo "    User 'grails' already exists."
 fi
