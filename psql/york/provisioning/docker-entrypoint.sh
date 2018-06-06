117bff30a0fe69844ad8faf43ff81bdaf2cf75cc
Ioannis Giakoumidis <ioannis.giakoumidis@york.ac.uk>
2018-06-06 11:40:55 +0100

    Create only a user and a schema on DB init

--- psql/york/provisioning/docker-entrypoint.sh
+++ psql/york/provisioning/docker-entrypoint.sh
@@ -3,19 +3,32 @@ set -e
 
 POSTGRES="psql -v ON_ERROR_STOP=1 -U ${POSTGRES_USER} -d ${POSTGRES_DB}"
 
-echo "==> Creating schema: ${SCHEMA}"
+echo "==> Revoking create on public schema from public"
+$POSTGRES <<-EOSQL
+  REVOKE CREATE ON SCHEMA PUBLIC FROM PUBLIC;
+EOSQL
+
+echo "==> Revoking create on schema public from ${POSTGRES_DB} database"
+$POSTGRES <<-EOSQL
+  REVOKE CREATE ON SCHEMA PUBLIC FROM ${POSTGRES_DB};
+EOSQL
+
+echo "==> Revoking all privileges on ${POSTGRES_DB} database from public"
+$POSTGRES <<-EOSQL
+  REVOKE ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} FROM PUBLIC;
+EOSQL
+
+echo "==> Creating schema ${SCHEMA}"
 $POSTGRES <<-EOSQL
   CREATE SCHEMA IF NOT EXISTS ${SCHEMA} AUTHORIZATION ${POSTGRES_USER}
 EOSQL
 
-echo "==> Creating role: ${APPLICATION}_role"
+echo "==> Alter user ${POSTGRES_USER} so he can create new users"
 $POSTGRES <<-EOSQL
-  CREATE ROLE ${APPLICATION}_role;
+  ALTER USER ${POSTGRES_USER} CREATEUSER;
 EOSQL
 
-echo "==> Creating user: ${APPLICATION}_1"
+echo "==> Alter user ${POSTGRES_USER} so he can create new roles"
 $POSTGRES <<-EOSQL
-  CREATE ROLE ${APPLICATION}_1
-  LOGIN PASSWORD '${POSTGRES_PASSWORD}'
-  IN ROLE ${APPLICATION}_role;
+  ALTER USER ${POSTGRES_USER} CREATEROLE;
 EOSQL
