e15e995c67ad124dc95f633956a2f0acddf99932
Ioannis Giakoumidis <ioannis.giakoumidis@york.ac.uk>
2018-06-23 12:36:24 +0100

    Create users and grand privileges for psql york image

--- psql/york/provisioning/docker-entrypoint.sh
+++ psql/york/provisioning/docker-entrypoint.sh
@@ -32,3 +32,41 @@ echo "==> Alter user ${POSTGRES_USER} so he can create new roles"
 $POSTGRES <<-EOSQL
   ALTER USER ${POSTGRES_USER} CREATEROLE;
 EOSQL
+
+echo "==> Create ${APPLICATION}_role and grant connect privilege"
+$POSTGRES <<-EOSQL
+  CREATE ROLE ${APPLICATION}_role;
+  GRANT CONNECT ON DATABASE ${POSTGRES_DB} TO  ${APPLICATION}_role;
+EOSQL
+
+echo "==> Create ${SCHEMA}_read_role and grant connect privilege"
+$POSTGRES <<-EOSQL
+  CREATE ROLE ${SCHEMA}_read_role;
+  GRANT CONNECT ON DATABASE ${POSTGRES_DB} TO ${SCHEMA}_read_role;
+EOSQL
+
+echo "==> Create ${APPLICATION}_1 user"
+$POSTGRES <<-EOSQL
+  CREATE ROLE ${APPLICATION}_1
+  LOGIN PASSWORD '${APPLICATION_USER_PWD}'
+  IN ROLE ${APPLICATION}_role;
+EOSQL
+
+echo "==> Grant usage on schema ${SCHEMA} to ${APPLICATION}_role and ${SCHEMA}_read_role"
+$POSTGRES <<-EOSQL
+  GRANT USAGE ON SCHEMA ${SCHEMA} TO ${APPLICATION}_role;
+  GRANT USAGE ON SCHEMA ${SCHEMA} TO ${SCHEMA}_read_role;
+EOSQL
+
+echo "==> Grant select on all tables in schema ${SCHEMA} to ${SCHEMA}_read_role"
+$POSTGRES <<-EOSQL
+  GRANT SELECT
+  ON ALL TABLES IN SCHEMA ${SCHEMA}
+  TO ${SCHEMA}_read_role;
+EOSQL
+
+echo "==> Set the search path for ${SCHEMA}_read_role and ${APPLICATION}_role"
+$POSTGRES <<-EOSQL
+  ALTER ROLE ${APPLICATION}_role SET search_path TO ${SCHEMA};
+  ALTER ROLE ${SCHEMA}_read_role SET search_path TO ${SCHEMA};
+EOSQL
